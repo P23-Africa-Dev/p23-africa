0b4ed2013c8b8ca7f21e47121c02ad44c35d796a
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-04-30 23:09:47 +0100

    working on responsive fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,31 +6,35 @@
     <section class="hero-section">
         <div class="hero-overlay"></div>
         <div class="container hero-content">
-            <div class="row">
-                <div class="col-md-8">
+            <div class="hero-content-block" >
+                <div class="left-block" >
                     <!-- Visual Line -->
                     <div class="line-connector"></div>
 
                     <!-- Highlighted Box -->
-                    <div class="main-box">
+                    {{-- <div class="main-box"> --}}
+                    <div class="">
                         <h6>KNOW ABOUT US</h6>
                         <h1><b>EMPOWERING BUSINESSES.<br>TRANSFORMING MARKETS</b></h1>
                     </div>
 
                     <!-- Subdescription -->
-                    <p class="descriptions">
+                    <p class="">
+                    {{-- <p class="descriptions"> --}}
                         P23 Africa is a team of seasoned business consultants dedicated to helping SMEs and entrepreneurs thrive in
                         competitive markets across Africa and the UK.
                     </p>
 
                     <!-- Contact Button -->
-                    <a href="#contact" class="contact-btn">
+                    <a href="#contact" class="">
+                    {{-- <a href="#contact" class="contact-btn"> --}}
                         Contact Us <i class="fas fa-arrow-right"></i>
                     </a>
                 </div>
-                <div class="col-md-4 text-left">
+                {{-- <div class="right-block text-left" > --}}
+                <div class=" text-left" >
                     <!-- Secondary Label -->
-                    <div class="info-tag">
+                    <div class="">
                         EXPANSION, BRAND, SALES
                     </div>
                 </div>
@@ -38,7 +42,7 @@
         </div>
     </section>
 
-    <section id="mission_vision">
+   <section id="mission_vision">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
@@ -61,7 +65,7 @@
         </div>
     </section>
 
-    <section class="growth-section">
+    {{--  <section class="growth-section">
         <div class="container">
             <h5>Our Three pillars to ensure success</h5>
             <h2>Empowering your growth <br> through;</h2>
@@ -107,8 +111,7 @@
             <div class="row g-0 align-items-stretch">
 
                 <img src="{{ asset('images/Commitment.png') }}" alt="">
-              <!-- Left Section -->
-              {{-- <div class="col-lg-6 commitment-box">
+              <div class="col-lg-6 commitment-box">
                 <div class="py-4 px-5">
                     <h4>OUR COMMITMENT</h4>
                     <p class="mt-4">
@@ -117,7 +120,6 @@
                 </div>
               </div>
         
-              <!-- Right Section -->
               <div class="col-lg-6 position-relative">
                 <img src="{{ asset('images/about.jpg') }}" alt="Team discussion" class="team-image" />
         
@@ -126,13 +128,12 @@
                   <br><br>
                   <p><strong>P23 Africa.</strong></p>
                 </div>
-              </div> --}}
+              </div>
             </div>
           </div>
     </section>
 
 
-        <!-- Stats Section -->
         <section class="container stats-about">
             <div class="stats-section-about">
                 <div class="container me-auto ms-auto w-50">
@@ -157,5 +158,5 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --}}
 @endsection
