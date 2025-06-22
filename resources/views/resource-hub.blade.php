9df9bbf5c87cb72c1449a9f76bd149e770ea7487
Dnurudeen <danijunurdeen@gmail.com>
2025-06-22 15:27:38 +0100

    commit revert for mobile

--- resources/views/resource-hub.blade.php
+++ resources/views/resource-hub.blade.php
@@ -73,21 +73,21 @@
                     <div class="carousel-container">
                         <div class="carousel-inner-custom" id="carouselSlides">
                             <div class="carousel-item-custom">
-                                <div class="team-card">
-                                    <div class="team-name">Victor<br>Adeniran</div>
-                                    <img src="{{ asset('images/victor_blog.png') }}" loading='lazy' alt="Person 1">
+                                <div class="team-card first">
+                                    <div class="team-name">Name<br>Surname</div>
+                                    <img src="{{ asset('images/first-mind.png') }}" loading='lazy' alt="Person 1">
                                 </div>
                             </div>
                             <div class="carousel-item-custom">
                                 <div class="team-card mid">
-                                    <div class="team-name">Margaret<br>Agwu</div>
-                                    <img src="{{ asset('images/margaret_blog.png') }}" loading='lazy' alt="Person 2">
+                                    <div class="team-name">Name<br>Surname</div>
+                                    <img src="{{ asset('images/second-mind.png') }}" loading='lazy' alt="Person 2">
                                 </div>
                             </div>
                             <div class="carousel-item-custom">
                                 <div class="team-card">
-                                    <div class="team-name">Motunrayo<br>Ajagbonna</div>
-                                    <img src="{{ asset('images/motunrayo_blog.png') }}" loading='lazy' alt="Person 3">
+                                    <div class="team-name">Name<br>Surname</div>
+                                    <img src="{{ asset('images/third-mind.png') }}" loading='lazy' alt="Person 3">
                                 </div>
                             </div>
                         </div>
